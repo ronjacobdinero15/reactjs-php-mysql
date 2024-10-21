985c5d89a3c6fc11c90b205374305547f5bcd61d
Ron Jacob Dinero <ronjacobdinero15@gmail.com>
2024-10-21 11:36:19 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import UserList from './component/UserList'
-import Add from './component/Add'
-import Edit from './component/Edit'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import ShelvesList from './pages/ShelvesList'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AddShelf from './pages/AddShelf'
+import EditShelf from './pages/EditShelf'
+import ShelfList from './pages/ShelfList'
+import AddItem from './pages/AddItem'
+import EditItem from './pages/EditItem'
 
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <div className="container">
       <h1 className="mt-5 mb-5 text-center">
@@ -13,13 +22,18 @@ function App() {
           <span className="text-primary">Create Delete Data API - 8</span>
         </b>
       </h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<UserList />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/edit/:user_id" element={<Edit />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ShelvesList />} />
+            <Route path="/addShelf" element={<AddShelf />} />
+            <Route path="/addItem/shelf/:shelf_id" element={<AddItem />} />
+            <Route path="/edit/shelf/:shelf_id" element={<EditShelf />} />
+            <Route path="/edit/item/:item_id" element={<EditItem />} />
+            <Route path="/shelf/:shelf_id" element={<ShelfList />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   )
 }
